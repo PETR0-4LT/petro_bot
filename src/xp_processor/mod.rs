0b059d13348500dcb91d8aa6349bc2c479a578af
PETR0-4LT <shield.techeds@gmail.com>
2026-04-12 15:24:22 +0300

    changed xp gain rate
    len() / 8 --> len() / 16;
    
    Bandaid patch for now. Will have a way to configure this through discord later

--- src/xp_processor/mod.rs
+++ src/xp_processor/mod.rs
@@ -1,18 +1,19 @@
 use serenity::all::Message;
-
 use crate::db_manager;
+
+const XP_DIVISOR: u32 = 16;
+const MIN_XP_THRESHOLD: u32 = 10;
 
 pub fn message_is_in_rp(src: &Message) -> bool {
     db_manager::channels::query_is_rp(&src.channel_id.to_string())
 }
 pub fn get_bonus(src: &str) -> u32 {
-    src.len() as u32 / 8 // DECIDE EXACT RATE LATER
+    src.len() as u32 / XP_DIVISOR
 }
-
 pub fn run(src: &Message) {
     if message_is_in_rp(src) {
         let bonus = get_bonus(&src.content);
-        if bonus >= 15 {
+        if bonus >= MIN_XP_THRESHOLD {
             db_manager::users::update_or_insert(&src.author.id.to_string(), bonus);
         }
     }
